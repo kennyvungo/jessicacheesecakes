8ce54b13c7a5c2c2564182385bcdafd1610b8bbf
Kenny <kennyvungo@gmail.com>
2024-03-07 20:09:57 -0800

    made events path

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div>Contact Form</div>
+    <div className='mt-20 w-screen h-3/5 bg-pink-600 flex flex-col justify-center items-center'>
+        Contact Form
+    </div>
   )
 }
 
--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className="flex flex-row fixed items-center bg-orange h-1/6 bg-blue-500 text-[#3B3B3B] font-sans w-screen z-10 min-h-24 sm:min-h-10">
+    <div className="flex flex-row fixed items-center bg-orange h-1/6 bg-blue-500 text-[#3B3B3B] font-sans w-screen z-10 min-h-24 sm:min-h-10 justify-around">
       <Link href="/"> HOME</Link>
       <Link href="/contact"> CONTACT</Link>
     </div>
