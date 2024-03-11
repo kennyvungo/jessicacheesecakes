ec1b32e1d11f1e8a7029637419f2d840353ba160
Kenny <kennyvungo@gmail.com>
2024-03-11 13:57:09 -0700

    added new header

--- components/header.tsx
+++ components/header.tsx
@@ -3,9 +3,15 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className="flex flex-row fixed items-center bg-orange h-1/6 bg-blue-500 text-[#3B3B3B] font-sans w-screen z-10 min-h-24 sm:min-h-10 justify-around">
+    <div className="flex flex-col fixed items-center bg-orange h-1/6 bg-blue-500 text-[#3B3B3B] font-sans w-screen z-10 min-h-24 sm:min-h-10 justify-around">
+      <div>
+        <div className="w-3/5 h-1/5">Moo Moo's Cheesecakes (logo)</div>
+      </div>
+      <div className='flex flex-row justify-between w-3/5'>
       <Link href="/"> HOME</Link>
+      <Link href="/about">ABOUT</Link>
       <Link href="/contact"> CONTACT</Link>
+      </div>
     </div>
   );
 }
